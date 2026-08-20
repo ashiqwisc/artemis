add51f47ec9edd43c6e0da9756048be7fc9de1c1
ashiqwisc <ashiq@wisc.edu>
2026-08-20 13:58:08 -0400

    additional scripts for dataset generation

---
+++ additional_scripts/piecewise_poisson.py
@@ -0,0 +1,19 @@
+
+def main(): 
+    # Seed 
+
+    # Init storage (a = \rho, u = \phi)
+
+    # For m = 1, ..., N: 
+        # sample a_1^{(m)}, a_2^{(m)}, T_m 
+
+        # Construct a from these 
+
+        # Run Poisson solver via mpirun to obtain u
+        
+        # Save (a_1^{(m)}, a_2^{(m)}, T_m, u) to storage 
+    
+    return 0
+
+if __name__ == "main": 
+    main() 
